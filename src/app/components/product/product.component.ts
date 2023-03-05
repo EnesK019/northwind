7f8e0ab5ecc969319d9db69a7d5c1fc21f367006
EnesK019 <eneskrtnc.19@hotmail.com>
2023-03-05 17:35:41 +0300

    05.03.2023

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr/toastr/toastr.service';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent {
+  products: Product[] = [];
+  dataLoaded = false;
+  filterText="";
+
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, ) {}//activatedroute 
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }
+      else{
+        this.getProducts()
+      }
+    }) 
+
+    
+  }
+
+  getProducts() {    
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
+
+  getProductsByCategory(categoryId:number) {    
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })   
+  }
+  
+  addToCart(product:Product){
+    //this.toastrService.success("Sepete Eklendi", product.productName)
+  }
+
+
+}
